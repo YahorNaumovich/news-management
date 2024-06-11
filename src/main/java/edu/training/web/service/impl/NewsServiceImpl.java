11405e3dede4877527e22522337eaeb1deddc138
YahorNaumovich <egor.n.naumovich@gmail.com>
2024-06-11 22:37:40 +0300

    Added logging for service methods

--- src/main/java/edu/training/web/service/impl/NewsServiceImpl.java
+++ src/main/java/edu/training/web/service/impl/NewsServiceImpl.java
@@ -10,65 +10,114 @@ import edu.training.web.service.NewsService;
 import edu.training.web.service.ServiceException;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NewsServiceImpl implements NewsService {
 
     private final NewsDao newsDao = DaoProvider.getInstance().getNewsDao();
 
+    private static final Logger LOGGER = Logger.getLogger(NewsServiceImpl.class.getName());
+
     @Override
     public List<NewsTile> getLastNews() throws ServiceException {
+
+        LOGGER.log(Level.INFO, "Method getLastNews is called");
+
         try {
+
             return newsDao.getTiles();
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during getting last news", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public List<Article> getArticles() throws ServiceException {
+
+        LOGGER.log(Level.INFO, "Method getArticles is called");
+
         try {
+
             return newsDao.getArticles();
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during getting articles", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public Article getArticleById(int articleId) throws ServiceException {
+
+        LOGGER.log(Level.INFO, "Method getArticleById is called");
+
         try {
+
             return newsDao.getArticleById(articleId);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during getting article by id", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public void addArticle(AddArticleInfo addArticleInfo) throws ServiceException {
 
+        LOGGER.log(Level.INFO, "Method addArticle is called");
+
         try {
+
             newsDao.addArticle(addArticleInfo);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during adding article", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public void editArticle(AddArticleInfo addArticleInfo, int articleId) throws ServiceException {
 
+        LOGGER.log(Level.INFO, "Method editArticle is called");
+
         try {
+
             newsDao.editArticle(addArticleInfo, articleId);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during editing article", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public void deleteArticle(int articleId) throws ServiceException {
 
+        LOGGER.log(Level.INFO, "Method deleteArticle is called");
+
         try {
+
             newsDao.deleteArticle(articleId);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during deleting article", e);
             throw new ServiceException(e);
+
         }
     }
 }
--- src/main/java/edu/training/web/service/impl/UserServiceImpl.java
+++ src/main/java/edu/training/web/service/impl/UserServiceImpl.java
@@ -11,20 +11,31 @@ import edu.training.web.service.UserRoles;
 import edu.training.web.service.UserService;
 
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserServiceImpl implements UserService {
 
     private final AuthenticationDao authenticationDao = DaoProvider.getInstance().getAuthenticationDao();
 
+    private static final Logger LOGGER = Logger.getLogger(UserServiceImpl.class.getName());
+
     @Override
     public User signIn(AuthenticationInfo authenticationInfo) throws ServiceException {
+
+        LOGGER.log(Level.INFO, "Method signIn is called");
 
         User user;
 
         try {
+
             user = authenticationDao.signInUser(authenticationInfo);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during signing in user", e);
             throw new ServiceException(e);
+
         }
 
         return user;
@@ -32,6 +43,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User signUp(UserRegistrationInfo userRegistrationInfo) throws ServiceException {
+
+        LOGGER.log(Level.INFO, "Method signUp is called");
 
         if (!userRegistrationInfo.getPassword().equals(userRegistrationInfo.getConfirmPassword())) {
             throw new ServiceException("Passwords do not match");
@@ -50,7 +63,10 @@ public class UserServiceImpl implements UserService {
         try {
             user = authenticationDao.signUpUser(userRegistrationInfo);
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during signing up user", e);
             throw new ServiceException(e);
+
         }
 
         return user;
@@ -59,44 +75,75 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUser(int id) throws ServiceException {
 
+        LOGGER.log(Level.INFO, "Method deleteUser is called");
+
         try {
+
             authenticationDao.deleteUser(id);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during deleting user", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public void changeUserRole(int userId, UserRoles role) throws ServiceException {
+
+        LOGGER.log(Level.INFO, "Method changeUserRole is called");
+
         try {
+
             int roleId = authenticationDao.getUserRoleId(role.toString());
             authenticationDao.changeUserRole(userId, roleId);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during changing user role", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public void changeUserPassword(int id, String newPassword, String confirmPassword) throws ServiceException {
+
+        LOGGER.log(Level.INFO, "Method changeUserPassword is called");
+
         if (!newPassword.equals(confirmPassword)) {
             throw new ServiceException("Passwords do not match");
         }
+
         try {
+
             authenticationDao.changeUserPassword(id, newPassword);
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during changing user password", e);
             throw new ServiceException(e);
+
         }
     }
 
     @Override
     public Map<String, User> getAllUsers() throws ServiceException {
 
+        LOGGER.log(Level.INFO, "Method getAllUsers is called");
+
         Map<String, User> users;
 
         try {
+
             users = authenticationDao.getAllUsers();
+
         } catch (DaoException e) {
+
+            LOGGER.log(Level.INFO, "Service error occurred during getting all users", e);
             throw new ServiceException(e);
+
         }
 
         return users;
